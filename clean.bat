4b27944393b32500546136495cdf9778366a2b16
Nova136 <chiayihang@gmail.com>
2019-11-14 11:50:07 +0800

    Revert "WIP Search function"
    This reverts commit 7705cd8ad7aafdc4cdea873dd60c571049d47174.

--- clean.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-cd (path to project/android folder) && ^
-gradlew clean && ^
-cd (path to project root folder) && ^
-react-native run-android
--- key.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = '';
-// WEB APP KEY
