92e90574764de2fc5af94d421428806004b9fda9
markpang <1800591@sit.singaporetech.edu.sg>
2019-11-23 15:26:42 +0800

    Added shawn registeration

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,26 +6,30 @@ import {
   TouchableHighlight,
   View,
   Alert,
-  Button,
   TextInput,
   Picker,
   Image,
   StyleSheet
 } from 'react-native'
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
-
 import DateTimePicker from '@react-native-community/datetimepicker';
 import moment from 'moment';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Button } from 'react-native-elements';
+import {BACKEND_SERVER} from 'react-native-dotenv';
+
 
 export default class SignUp extends React.Component {
   state = {
-    username: '', password: '', email: '', confirmPassword: '', language: 'English',
+    username: '', password: '', email: '', confirmPassword: '', sex: 'Male', commuteMethod: 'Walking',
     modalVisible: false,
-    date: new Date('2019-10-25T14:42:42'),
+    emailErrorModalVisible: false,
+    invalidEmailErrorModalVisible: false,
+    date: new Date(),
     mode: 'date',
     show: false,
+    dataResponse: '',
+    results: {}
   }
 
   setDate = (event, date) => {
@@ -51,22 +55,80 @@ export default class SignUp extends React.Component {
   setModalVisible(visible) {
     this.setState({modalVisible: visible});
   }
+
+  setEmailErrorModalVisible(visible) {
+    this.setState({emailErrorModalVisible: visible});
+  }
+
+  setInvalidEmailErrorModalVisible(visible) {
+    this.setState({invalidEmailErrorModalVisible: visible});
+  }
+
   onChangeText = (key, val) => {
     this.setState({ [key]: val })
   }
   signUp = async () => {
-    const { username, password, email, language } = this.state
+
+    console.log('test')
+
+    const { username, password, email, sex, commuteMethod, date } = this.state
+
     try {
       // here place your signup logic
-      console.log('user successfully signed up!: ', success)
+      const url = 'http://' + BACKEND_SERVER + '/amble/auth/register'
+      const settings = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          name: username,
+          birthdate: date.toLocaleDateString(),
+          sex: sex,
+          commute_method: commuteMethod,
+        })
+      };
+      //await fetch(url, setting)
+      const data = await fetch(url, settings);
+
+      //      const data = await fetch(url2);
+
+      const data_response = await data.json();
+      //console.log(data_response)
+      this.setState({
+          results: data_response,
+      })
+
+
+      console.log(settings.body)
+      console.log(typeof(settings.body))
+      //this.setModalVisible(true);
+      console.log('user successfully signed up!: ', data_response)
+      this.props.navigation.navigate('SignIn');
+
     } catch (err) {
+      //this.setEmailErrorModalVisible(true);
       console.log('error signing up: ', err)
     }
+
+    if(this.state.results.error == "email already in use") {
+      this.setEmailErrorModalVisible(true);
+    } else if(this.state.results.error == "invalid email") {
+      this.invalidEmailErrorModalVisible(true);
+    } else {
+      this.setModalVisible(true);
+    }
+    console.log(this.state.results.result)
+
   }
-  signIn = async () => {
+
+  signIn() {
     this.props.navigation.navigate('SignIn');
   }
- //user, email, dob, preferred lang, password, confirm password, i agree, sign up button
+
   render() {
     const { show, date, mode } = this.state;
 
@@ -121,19 +183,35 @@ export default class SignUp extends React.Component {
         </View>
         <View style={styles.row}>
           <Text style={styles.text}>
-            Language:
+            Sex:
           </Text>
-          <Picker
-            selectedValue={this.state.language}
-            style={{height: 50, width: 250,}}
-            onValueChange={(itemValue, itemIndex) =>
-              this.setState({language: itemValue})
-            }>
-            <Picker.Item label="English" value="English" />
-            <Picker.Item label="Mandarin" value="Mandarin" />
-            <Picker.Item label="Malay" value="Malay" />
-            <Picker.Item label="Tamil" value="Tamil" />
-          </Picker>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={this.state.sex}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({sex: itemValue})
+              }>
+              <Picker.Item label="Male" value="Male" />
+              <Picker.Item label="Female" value="Female" />
+            </Picker>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            Commute Method:
+          </Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={this.state.commuteMethod}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({commuteMethod: itemValue})
+              }>
+              <Picker.Item label="Walking" value="Walking" />
+              <Picker.Item label="Cycling" value="Cycling" />
+              <Picker.Item label="Bus" value="Bus" />
+              <Picker.Item label="Mrt" value="Mrt" />
+            </Picker>
+          </View>
         </View>
         <View style={styles.row}>
           <Text style={styles.text}>
@@ -162,18 +240,14 @@ export default class SignUp extends React.Component {
           />
         </View>
         <Button
-          color="#1B879C"
+          buttonStyle={{backgroundColor: '#1B879C'}}
           title='Sign Up'
-          onPress={() => {
-            //this.goHome;
-            this.setModalVisible(true);
-          }}
+          onPress={this.signUp}
         />
         <Text style={styles.bottomText}>
         Already a member? <Text onPress={this.signIn}>
            Sign in here!
         </Text></Text>
-
 
         <Modal
           animationType="fade"
@@ -187,7 +261,54 @@ export default class SignUp extends React.Component {
               <Text>User successfully created!</Text>
 
               <TouchableHighlight>
-                <Button onPress={this.signIn}
+                <Button buttonStyle={{backgroundColor: '#1B879C'}}
+                onPress={this.signIn}
+                  title="Okay!"
+                  style={{backgroundColor: '#1B879C'}}
+                />
+              </TouchableHighlight>
+
+            </View>
+          </View>
+        </Modal>
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.emailErrorModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={styles.successModal}>
+            <View>
+              <Text>Email already in use!</Text>
+
+              <TouchableHighlight>
+                <Button buttonStyle={{backgroundColor: '#1B879C'}}
+                onPress={this.signIn}
+                  title="Okay!"
+                  style={{backgroundColor: '#1B879C'}}
+                />
+              </TouchableHighlight>
+
+            </View>
+          </View>
+        </Modal>
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.invalidEmailErrorModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={styles.successModal}>
+            <View>
+              <Text>Invalid email!</Text>
+
+              <TouchableHighlight>
+                <Button buttonStyle={{backgroundColor: '#1B879C'}}
+                onPress={this.signIn}
                   title="Okay!"
                   style={{backgroundColor: '#1B879C'}}
                 />
@@ -203,23 +324,31 @@ export default class SignUp extends React.Component {
 
 const styles = StyleSheet.create({
   text: {
-    width: 100,
-    fontSize: 18,
+    width: 70,
+    fontSize: 16,
     fontWeight: '500',
     color: '#000000',
     textAlign: 'left',
     paddingTop: 15,
   },
   input: {
-    width: 230,
-    height: 55,
+    width: 200,
+    height: 40,
     backgroundColor: '#FEFEFE',
     margin: 10,
     padding: 8,
     color: '#AAAAAA',
-    borderRadius: 18,
-    fontSize: 18,
-    fontWeight: '500',
+    borderRadius: 6,
+    fontSize: 16,
+  },
+  pickerContainer: {
+    width: 200,
+    height: 45,
+    backgroundColor: '#FEFEFE',
+    margin: 10,
+    color: '#AAAAAA',
+    borderRadius: 6,
+    fontSize: 16,
   },
   logo: {
     width: 100,
